9ce87342f0314d6a0c3b0c1dd62708aad7903f2d
jzarifis <giannis@aegeantaxi.com>
2021-02-05 11:43:48 +0200

    Add local dev network

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,6 @@
-var RecentBlockReward = artifacts.require("RecentBlockReward");
-var UserProfile = artifacts.require("UserProfile");
+var RecentValidators = artifacts.require("RecentValidators");
 var PaymentChannels = artifacts.require("PaymentChannels");
 module.exports = function(deployer, network, accounts) {
-    deployer.deploy(RecentBlockReward, accounts[0]);
-    deployer.deploy(UserProfile);
+    deployer.deploy(RecentValidators, ["0x02775b8ef251abc06950d3be4522d71d715bc03a"]);
     deployer.deploy(PaymentChannels);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -7,10 +7,19 @@ module.exports = {
       host: 'localhost',
       port: 8545
     },
+    recentlocal: {
+      provider: () => new HDWalletProvider("combine close before lawsuit asthma glimpse yard debate mixture stool adjust ride", "http://localhost:8545"),
+      from: "0x3d176d013550b48974c1d2f0b18c6df1ff71391e",
+      network_id: "12858955",
+      host: 'localhost',
+      port: 8545,
+      gasPrice: 1000000000,
+      confirmations: 0
+    },
     recent: {
       provider: () => new HDWalletProvider("combine close before lawsuit asthma glimpse yard debate mixture stool adjust ride", "http://ec2-3-124-182-37.eu-central-1.compute.amazonaws.com:8545"),
       from: "0x3d176d013550b48974c1d2f0b18c6df1ff71391e",
-      network_id: "12858966",
+      network_id: "12858956",
       host: 'http://ec2-3-124-182-37.eu-central-1.compute.amazonaws.com',
       port: 8545,
       gasPrice: 1000000000,
